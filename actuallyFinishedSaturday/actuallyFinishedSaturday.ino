cd76ea3fc07725973a0193106ee0b7d82526c4b6
jagen31 <jargen3d@yahoo.com>
2014-06-19 19:29:51 -0400

    Added LED Code

---
+++ actuallyFinishedSaturday/actuallyFinishedSaturday.ino
@@ -0,0 +1,395 @@
+byte theByte;
+byte datas;
+#include "LPD8806.h"
+#include "SPI.h"
+byte me = 1;
+int nLEDs = 16;
+
+int DpLeft = 13;
+int CpLeft = 12;
+
+int DpRight  = 11;
+int CpRight = 10;
+
+int DpBack  = 2;
+int CpBack = 1;
+
+int DpRightCollect = 4;
+int CpRightCollect = 3;
+
+int DpLeftCollect = 9;
+int CpLeftCollect = 8;
+
+LPD8806 strip = LPD8806(nLEDs, DpLeft, CpLeft);
+LPD8806 LeftStrips = LPD8806(nLEDs, DpLeft, CpLeft);
+LPD8806 RightStrips = LPD8806(nLEDs, DpRight, CpRight);
+LPD8806 BackStrips = LPD8806(nLEDs, DpBack, CpBack);
+LPD8806 RightCollectStrips = LPD8806(nLEDs, DpRightCollect, CpRightCollect);
+LPD8806 LeftCollectStrips = LPD8806(nLEDs, DpLeftCollect, CpLeftCollect);
+
+int i;
+
+void setup(){
+  Serial.begin(9600);
+  LeftStrips.begin();
+  RightStrips.begin();
+  BackStrips.begin();
+  RightCollectStrips.begin();
+  LeftCollectStrips.begin();
+  LeftStrips.show();
+  RightStrips.show();
+  BackStrips.show();
+  LeftCollectStrips.show();
+  RightCollectStrips.show();
+
+}
+
+//kill();
+//countdown();
+
+void loop(){
+
+  datas = digitalRead(5);         // Set 'data' to the reading at pin 4
+
+  bitWrite(theByte, 0, datas);    // Write a 0 or 1 to the first bit of 'data;
+
+  datas = digitalRead(6);     // Set 'data' to the reading at pin 5
+  bitWrite(theByte, 1, datas);    // Write a 0 or 1 to the second bit of 'data'
+
+  datas = digitalRead(7);          // Set 'data' to the reading at pin 6
+  bitWrite(theByte, 2, datas);    // Write a 0 or 1 to the third bit of 'data
+  Serial.println(theByte);
+  int taco = 7;
+
+  switch(taco){
+  case 0:
+    //  wave(strip.Color(0,127,0), 2, 40); // green icy
+    //  wave(strip.Color(0,127,0), 4, 50);  
+    wave(strip.Color(0,127,0), 2, 60); 
+
+    break;
+  case 1:
+    doubleWipe(strip.Color(0,127,0),strip.Color(127,127,127),50); //green n' white cross
+
+    break;
+  case 2:
+    fixedFlash();
+
+    break;
+  case 3:
+    scanner(0,127,0, 8); 
+
+    break;
+  case 4:
+    scanner(0,127,0, 15); 
+
+    break;
+  case 5:
+    colorWipe(strip.Color(127,127,127),50);
+
+    break; 
+  case 6:
+    colorWipe(strip.Color(0, 127,0), 0);
+
+    break;
+  case 7:
+    kill();
+
+    break;
+  }
+}
+void kill(){
+  int i;
+  for(i=0; i<strip.numPixels(); i++){
+    LeftStrips.setPixelColor(i,strip.Color(0,0,0));
+    RightStrips.setPixelColor(i,strip.Color(0,0,0));
+    BackStrips.setPixelColor(i,strip.Color(0,0,0));
+    LeftCollectStrips.setPixelColor(i,strip.Color(0,0,0));
+    RightCollectStrips.setPixelColor(i,strip.Color(0,0,0));
+  }
+  LeftStrips.show();
+  RightStrips.show();
+  BackStrips.show();
+  LeftCollectStrips.show();
+  RightCollectStrips.show();
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  int i;
+  for(i=0; i<strip.numPixels(); i++){
+    LeftStrips.setPixelColor(15 - i,c);
+    RightStrips.setPixelColor(i,c);
+    BackStrips.setPixelColor(i,c);
+    LeftCollectStrips.setPixelColor(i,c);
+    RightCollectStrips.setPixelColor(15 - i,c);
+
+    LeftStrips.show();
+    RightStrips.show();
+    BackStrips.show();
+    LeftCollectStrips.show();
+    RightCollectStrips.show();
+    
+    delay(wait);
+  }
+
+}
+
+
+void doubleWipe(uint32_t c, int cc, uint8_t wait){
+  int i;
+  int j = 15;
+  for (i=0; i < strip.numPixels(); i++) {
+    LeftStrips.setPixelColor(i, cc);
+    LeftStrips.setPixelColor(j-i,c);
+    LeftStrips.show();
+    
+    RightStrips.setPixelColor(i, c);
+    RightStrips.setPixelColor(j-i,cc);
+    RightStrips.show();
+    
+    BackStrips.setPixelColor(i, c);
+    BackStrips.setPixelColor(j-i,cc);
+    BackStrips.show();
+    
+    LeftCollectStrips.setPixelColor(i, c);
+    LeftCollectStrips.setPixelColor(j-i,cc);
+    LeftCollectStrips.show();
+    
+    RightCollectStrips.setPixelColor(i, cc);
+    RightCollectStrips.setPixelColor(j-i,c);
+    RightCollectStrips.show();
+    delay(wait);
+  }
+}
+
+void fixedFlash(){
+  int i;
+  for(i=0; i<strip.numPixels(); i++){
+    LeftStrips.setPixelColor(i,strip.Color(90,40,0));
+    RightStrips.setPixelColor(i,strip.Color(90,40,0));
+    BackStrips.setPixelColor(i,strip.Color(90,40,0));
+    LeftCollectStrips.setPixelColor(i,strip.Color(90,40,0));
+    RightCollectStrips.setPixelColor(i,strip.Color(90,40,0));
+  }
+  LeftStrips.show();
+  RightStrips.show();
+  BackStrips.show();
+  LeftCollectStrips.show();
+  RightCollectStrips.show();
+  
+  delay(200);
+  
+  for(i=0; i<strip.numPixels(); i++){
+    LeftStrips.setPixelColor(i,strip.Color(0,0,0));
+    RightStrips.setPixelColor(i,strip.Color(0,0,0));
+    BackStrips.setPixelColor(i,strip.Color(0,0,0));
+    LeftCollectStrips.setPixelColor(i,strip.Color(0,0,0));
+    RightCollectStrips.setPixelColor(i,strip.Color(0,0,0));
+  }
+  LeftStrips.show();
+  RightStrips.show();
+  BackStrips.show();
+  LeftCollectStrips.show();
+  RightCollectStrips.show();
+  
+  delay(200);
+}
+
+void yellowflash(){
+  int i;
+  int c=0;
+
+  for(int i=0; i<strip.numPixels();i++){
+    strip.setPixelColor(i,strip.Color(90,40,0));
+  }
+
+  strip.show();
+  for(int c = 100; c > 1; c--){
+    for(int i=0; i<strip.numPixels(); i++){
+      strip.setPixelColor(i,strip.Color(c,c/2,0));   
+    } 
+    delay(5);
+    strip.show();
+  }
+}
+#define PI 3.14159265
+void wave(uint32_t c, int cycles, uint8_t wait) {
+  float y;
+  byte  r, g, b, r2, g2, b2;
+
+  // Need to decompose color into its r, g, b elements
+  g = (c >> 16) & 0x7f;
+  r = (c >>  8) & 0x7f;
+  b =  c        & 0x7f; 
+
+  for(int x=0; x<(strip.numPixels()*5); x++)
+  {
+    for(int i=0; i<strip.numPixels(); i++) {
+      y = sin(PI * (float)cycles * (float)(x + i) / (float)strip.numPixels());
+      if(y >= 0.0) {
+        // Peaks of sine wave are white
+        y  = 1.0 - y; // Translate Y to 0.0 (top) to 1.0 (center)
+        r2 = 127 - (byte)((float)(127 - r) * y);
+        g2 = 127 - (byte)((float)(127 - g) * y);
+        b2 = 127 - (byte)((float)(127 - b) * y);
+      } 
+      else {
+        // Troughs of sine wave are black
+        y += 1.0; // Translate Y to 0.0 (bottom) to 1.0 (center)
+        r2 = (byte)((float)r * y);
+        g2 = (byte)((float)g * y);
+        b2 = (byte)((float)b * y);
+      }
+      LeftStrips.setPixelColor(15 - i, r2, g2, b2);
+      RightStrips.setPixelColor(i, r2, g2, b2);
+      BackStrips.setPixelColor(i, r2, g2, b2);
+      LeftCollectStrips.setPixelColor(i, r2, g2, b2);
+      RightCollectStrips.setPixelColor(15 - i, r2, g2, b2);
+    }
+    LeftStrips.show();
+    RightStrips.show();
+    BackStrips.show();
+    LeftCollectStrips.show();
+    RightCollectStrips.show();
+    delay(wait);
+  }
+}
+
+void flames(uint8_t wait, uint32_t r,uint32_t g,uint32_t b){
+  int variable = 3;
+  for(int i = 0; i <= strip.numPixels(); i++){
+    for(int red = 0; red < i; red++){
+      strip.setPixelColor(red,strip.Color(127,0,0));
+    }
+    for(int orange = 0; orange < i*2/3; orange++){
+      strip.setPixelColor(orange,strip.Color(117,20,0));
+    }
+    for(int yellow = 0; yellow < i/3; yellow++){
+      strip.setPixelColor(yellow,strip.Color(127,60,0));
+    }
+    strip.show();
+    delay(wait);
+  }
+
+  for(int i = 0; i <= strip.numPixels(); i++){
+    for(int red = i; red < strip.numPixels(); red++){
+      strip.setPixelColor(red,strip.Color(127,0,0));
+    }
+    for(int orange = i; orange < strip.numPixels()*2/3; orange++){
+      strip.setPixelColor(orange,strip.Color(117,20,0));
+    }
+    for(int yellow = i; yellow < strip.numPixels()/3; yellow++){
+      strip.setPixelColor(yellow,strip.Color(127,60,0));
+    }
+    strip.setPixelColor(i,strip.Color(0,0,0));
+    strip.show();
+    delay(wait);      
+  }   
+}
+void scanner(uint8_t r, uint8_t g, uint8_t b, uint8_t wait) {
+  int i, j, pos, dir;
+
+  pos = 0;
+  dir = 1;
+
+  for(i=0; i<((strip.numPixels()-1) * 8); i++) {
+    // Draw 5 pixels centered on pos.  setPixelColor() will clip
+    // any pixels off the ends of the strip, no worries there.
+    // we'll make the colors dimmer at the edges for a nice pulse
+    // look
+    LeftStrips.setPixelColor(15 - (pos - 2), strip.Color(r/4, g/4, b/4));
+    LeftStrips.setPixelColor(15 - (pos - 1), strip.Color(r/2, g/2, b/2));
+    LeftStrips.setPixelColor(15 - pos, strip.Color(r, g, b));
+    LeftStrips.setPixelColor(15 - (pos + 1), strip.Color(r/2, g/2, b/2));
+    LeftStrips.setPixelColor(15 - (pos + 2), strip.Color(r/4, g/4, b/4));
+    
+    RightStrips.setPixelColor(pos - 2, strip.Color(r/4, g/4, b/4));
+    RightStrips.setPixelColor(pos - 1, strip.Color(r/2, g/2, b/2));
+    RightStrips.setPixelColor(pos, strip.Color(r, g, b));
+    RightStrips.setPixelColor(pos + 1, strip.Color(r/2, g/2, b/2));
+    RightStrips.setPixelColor(pos + 2, strip.Color(r/4, g/4, b/4));
+    
+    BackStrips.setPixelColor(pos - 2, strip.Color(r/4, g/4, b/4));
+    BackStrips.setPixelColor(pos - 1, strip.Color(r/2, g/2, b/2));
+    BackStrips.setPixelColor(pos, strip.Color(r, g, b));
+    BackStrips.setPixelColor(pos + 1, strip.Color(r/2, g/2, b/2));
+    BackStrips.setPixelColor(pos + 2, strip.Color(r/4, g/4, b/4));
+    
+    LeftCollectStrips.setPixelColor(pos - 2, strip.Color(r/4, g/4, b/4));
+    LeftCollectStrips.setPixelColor(pos - 1, strip.Color(r/2, g/2, b/2));
+    LeftCollectStrips.setPixelColor(pos, strip.Color(r, g, b));
+    LeftCollectStrips.setPixelColor(pos + 1, strip.Color(r/2, g/2, b/2));
+    LeftCollectStrips.setPixelColor(pos + 2, strip.Color(r/4, g/4, b/4));
+    
+    RightCollectStrips.setPixelColor(15 - (pos - 2), strip.Color(r/4, g/4, b/4));
+    RightCollectStrips.setPixelColor(15 - (pos - 1), strip.Color(r/2, g/2, b/2));
+    RightCollectStrips.setPixelColor(15 - pos, strip.Color(r, g, b));
+    RightCollectStrips.setPixelColor(15 - (pos + 1), strip.Color(r/2, g/2, b/2));
+    RightCollectStrips.setPixelColor(15 - (pos + 2), strip.Color(r/4, g/4, b/4));
+
+    LeftStrips.show();
+    RightStrips.show();
+    BackStrips.show();
+    LeftCollectStrips.show();
+    RightCollectStrips.show();
+    delay(wait);
+    // If we wanted to be sneaky we could erase just the tail end
+    // pixel, but it's much easier just to erase the whole thing
+    // and draw a new one next time.
+    for(int i = 0; i < strip.numPixels(); i++){
+      LeftStrips.setPixelColor(i,strip.Color(0,0,0));
+      RightStrips.setPixelColor(i,strip.Color(0,0,0));
+      BackStrips.setPixelColor(i,strip.Color(0,0,0));
+      LeftCollectStrips.setPixelColor(i,strip.Color(0,0,0));
+      RightCollectStrips.setPixelColor(i,strip.Color(0,0,0));
+    }
+    // Bounce off ends of strip
+    pos += dir;
+    if(pos < 0) {
+      pos = 1;
+      dir = -dir;
+    } 
+    else if(pos >= strip.numPixels()) {
+      pos = strip.numPixels() - 2;
+      dir = -dir;
+    }
+  }
+}
+
+void ProgressBar(){
+
+  for(int i=0; i<strip.numPixels(); i++){
+    strip.setPixelColor(i, strip.Color(127,127,127));
+    strip.show();
+    delay(1000/strip.numPixels());
+  }
+  while(true){
+
+    for(int i=0;i<strip.numPixels(); i++){
+      strip.setPixelColor(i, strip.Color(0,0,0));
+
+    }
+    strip.show();
+
+    delay(75);
+    for(int i=0;i<strip.numPixels(); i++){
+      strip.setPixelColor(i, strip.Color(127,127,127));
+
+    }
+    strip.show();
+
+    delay(75);
+
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
---
+++ demo_code/demo_code.ino
@@ -0,0 +1,547 @@
+#include "LPD8806.h"
+#include "SPI.h"
+byte me = 1;
+
+int nLEDs = 16;
+
+int dataPinLeft = 13;
+int clockPinLeft = 12;
+
+int dataPinRight  = 11;
+int clockPinRight = 10;
+
+int dataPinBack  = 9;
+int clockPinBack = 8;
+
+int dataPinRightCollector = 4;
+int clockPinRightCollector = 3;
+
+int dataPinLeftCollector = 2;
+int clockPinLeftCollector = 1;
+
+LPD8806 leftstrips = LPD8806(nLEDs, dataPinLeft, clockPinLeft);
+LPD8806 rightstrips = LPD8806(nLEDs, dataPinRight, clockPinRight);
+LPD8806 backstrips = LPD8806(nLEDs, dataPinBack, clockPinBack);
+LPD8806 rightCollectorstrips = LPD8806(nLEDs, dataPinRightCollector,clockPinRightCollector);
+LPD8806 leftCollectorstrips = LPD8806(nLEDs,dataPinLeftCollector,clockPinRightCollector);
+
+LPD8806 strips[6];
+
+void setup(){
+  strips[0] = leftCollectorstrips;
+  strips[1] = leftstrips;
+  strips[2] = backstrips;
+  strips[3]=rightstrips;
+  strips[4] = rightCollectorstrips;
+  for(int a = 0; a<5;a++){
+  strips[a].begin();
+  strips[a].show();
+  }
+  //void dither(uint32_t c, uint8_t wait);
+
+  // void scanner(uint8_t r, uint8_t g, uint8_t b, uint8_t wait);
+  //void wave(uint32_t c, int cycles, uint8_t wait);
+  //uint32_t Wheel(uint16_t WheelPos);
+  //void launchLight(uint32_t,uint32_t,uint8_t);
+
+}
+void loop(){
+  
+  switch(me){   
+  case 1: 
+  for(int a = 0 ; a <5;a++){
+    colorWipe(strips[a].Color(  127, 127,   127), 50);  // Green
+    colorWipe(strips[a].Color(  0, 127,   0), 50);  // WHITE
+    colorWipe(strips[a].Color(  0, 0,   0), 1);  // BLANK
+  }break;
+  case 2:
+  for(int a = 0 ; a <5;a++){
+    doubleWipe(strips[a],strips[a].Color(127,0,0),strips[a].Color(0,0,127),50);
+    doubleWipe(strips[a],strips[a].Color(0,127,0),strips[a].Color(127,127,127),50);
+    colorWipe(strips[a].Color(  0, 0,   0), 1);  // BLANK  
+  }break;
+  case 3:
+  for(int a = 0 ; a <5;a++){
+    dither(strips[a],strips[a].Color(0,127,0), 15);// green
+    dither(strips[a],strips[a].Color(127,127,127), 15);// white
+    colorWipe(strips[a].Color(  0, 0,   0),1);  // BLANK
+  }break;
+  case 4:
+  for(int a = 0 ; a <5;a++){
+    scanner(strips[a],0,127,0, 8);        // green
+    colorWipe(strips[a].Color(  0, 0,   0), 1);  // BLANK
+  }break;
+  case 5:
+  for(int a = 0;a<5;a++){
+    theaterChase(strips[a],strips[a].Color(127, 127, 127), 50); // White
+    theaterChase(strips[a],strips[a].Color(0, 127, 0), 50); // green
+    colorWipe(strips[a].Color(  0, 0,   0), 1);  // BLANK
+  }break;
+  case 6:
+  for(int a = 0;a<5;a++){
+    wave(strips[a].Color(0,127,127), 2, 40); // green icy
+    colorWipe(strips[a].Color(  0, 0,   0), 1);  // BLANK
+  }break;
+  case 7:
+    randomRainbow(500,5);
+
+    break;
+  case 8:
+//  scannerPAC(2,40);
+  launchLight(strips[a],strips.Color(127,0,0),40);
+  launchLight(strips[a],strips.Color(127,10,0),40);
+  colorWipe(strips.Color(0,0,0),1); 
+  Wipe4(strips[a],100,strips.Color(0,127,0),strips.Color(127,127,127));
+  Wipe4(strips[a],100,strips.Color(0,0,127),strips.Color(127,0,0)); 
+  colorWipe(strips.Color(0,0,0),1);
+  
+  binaryCount(20,1000,strips.Color(0,127,0));
+  colorWipe(strips.Color(0,0,0),1);  */
+
+    break;
+  }
+   me++;
+  if(me >8){
+    me = 0;
+  }
+  colorChase(strips.Color(0, 127, 0), 50); // Green
+  colorChase(strips.Color(127, 127, 127), 50); // White
+
+
+
+
+
+}
+
+int dither(LPD8806 strips, uint32_t c, uint8_t wait) {
+  int hiBit = 0;
+  int n = strips.numPixels() - 1;
+  for(int bit=1; bit < 0x8000; bit <<= 1){
+    if(n & bit) hiBit = bit;
+  }
+  int bit, reverse;
+  for(int i=0; i<(hiBit << 1); i++) {
+    reverse = 0;
+    for(bit=1; bit <= hiBit; bit <<= 1) {
+      reverse <<= 1;
+      if(i & bit) reverse |= 1;
+    }
+    strips.setPixelColor(reverse, c);
+    strips.show();
+    d1elay(wait);
+  }
+  delay(250);
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  int i;
+
+  for (i=0; i < strips.numPixels(); i++) {
+    strips.setPixelColor(i, c);
+    strips.show();
+    delay(wait);
+  }
+}
+
+void colorChase(LPD8806 strips, uint32_t c, uint8_t wait) {
+  int i;
+  for(1i=0; i<strips.numPixels(); i++) strips.setPixelColor(i, 0);
+  for(i=0; i<strips.numPixels(); i++) {
+    strips.setPixelColor(i, c); // Set new pixel 'on'
+    strips.show();              // Refresh LED states
+    strips.setPixelColor(i, 0); // Erase pixel, but don't refresh!
+    delay(wait);
+  }
+}
+//THECODEFORBOBBYBYHIREN
+void launchLight(uint32_t c , uint8_t wait){
+  int i;
+  int h=15;
+  for(i=0; i<3; i++) strips.setPixelColor(i,0);
+  for(i=0; i<3; i++){
+    strips.setPixelColor(i,c);
+    strips.show();
+    strips.setPixelColor(i,0);
+    delay(wait);
+  }
+}
+
+void fire(LPD8806 strips, uint32_t c , uint8_t wait){
+  int i;
+  for(i=3; i<strips.numPixels(); i++) strips.setPixelColor(i,0);
+  for(i=3; i<strips.numPixels(); i++){
+    strips.setPixelColor(i,c);
+    strips.show();
+    strips.setPixelColor(i,0);
+    delay(wait);
+  }
+  
+
+  for(i=3; i<strips.numPixels(); i++) strips.setPixelColor(i,0);
+  for(i=3; i<strips.numPixels(); i++){
+    strips.setPixelColor(i,c);
+    strips.show();
+    strips.setPixelColor(i,0);
+    delay(wait);
+  }
+}
+
+void theaterChase(LPD8806 strips, uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (int i=0; i < strips.numPixels(); i=i+3) {
+        strips.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strips.show();
+
+      delay(wait);
+
+      for (int i=0; i < strips.numPixels(); i=i+3) {
+        strips.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+void scanner(LPD8806 strips, uint8_t r, uint8_t g, uint8_t b, uint8_t wait) {
+  int i, j, pos, dir;
+
+  pos = 0;
+  dir = 1;
+
+  for(i=0; i<((strips.numPixels()-1) * 8); i++) {
+    // Draw 5 pixels centered on pos.  setPixelColor() will clip
+    // any pixels off the ends of the strips, no worries there.
+    // we'll make the colors dimmer at the edges for a nice pulse
+    // look
+    strips.setPixelColor(pos - 2, strips.Color(r/4, g/4, b/4));
+    strips.setPixelColor(pos - 1, strips.Color(r/2, g/2, b/2));
+    strips.setPixelColor(pos, strips.Color(r, g, b));
+    strips.setPixelColor(pos + 1, strips.Color(r/2, g/2, b/2));
+    strips.setPixelColor(pos + 2, strips.Color(r/4, g/4, b/4));
+
+    strips.show();
+    delay(wait);
+    // If we wanted to be sneaky we could erase just the tail end
+    // pixel, but it's much easier just to erase the whole thing
+    // and draw a new one next time.
+    for(j=-2; j<= 2; j++) 
+      strips.setPixelColor(pos+j, strips.Color(0,0,0));
+    // Bounce off ends of strips
+    pos += dir;
+    if(pos < 0) {
+      pos = 1;
+      dir = -dir;
+    } 
+    else if(pos >= strips.numPixels()) {
+      pos = strips.numPixels() - 2;
+      dir = -dir;
+    }
+  }
+}
+
+// Sine wave effect
+#define PI 3.14159265
+void wave(uint32_t c, int cycles, uint8_t wait) {
+  float y;
+  byte  r, g, b, r2, g2, b2;
+
+  /* Need to decompose color into its r, g, b elements
+   g = (c >> 16) & 0x7f;
+   r = (c >>  8) & 0x7f;
+   b =  c        & 0x7f; 
+   */
+  g = (c >> 16) | 0;
+  r = (c >> 8)  | 0x0;
+  b = c         | 0x00;
+  for(int x=0; x<(strips.numPixels()*5); x++)
+  {
+    for(int i=0; i<strips.numPixels(); i++) {
+      y = sin(PI * (float)cycles * (float)(x + i) / (float)strips.numPixels());
+      if(y >= 0.0) {
+        // Peaks of sine wave are white
+        y  = 1.0 - y; // Translate Y to 0.0 (top) to 1.0 (center)
+        r2 = 127 - (byte)((float)(127 - r) * y);
+        g2 = 127 - (byte)((float)(127 - g) * y);
+        b2 = 127 - (byte)((float)(127 - b) * y);
+      } 
+      else {
+        // Troughs of sine wave are black
+        y += 1.0; // Translate Y to 0.0 (bottom) to 1.0 (center)
+        r2 = (byte)((float)r * y);
+        g2 = (byte)((float)g * y);
+        b2 = (byte)((float)b * y);
+      }
+      strips.setPixelColor(i, r2, g2, b2);
+    }
+    strips.show();
+    delay(wait);
+  }
+}
+
+
+//Jason's crazy cool code
+void randomRainbow(int cycles,uint8_t wait){
+  int j;
+  for (j=0; j < cycles; j++) {
+    // 5 cycles of all 384 colors in the wheel
+    strips.setPixelColor(random(0,16), strips.Color(random(0,2)*127,random(0,2)*127,random(0,2)*127));
+    // write all the pixels out
+    strips.show();
+    delay(wait);
+  }
+}*/
+
+void Wipe4(LPD8806 strips, uint8_t wait, uint32_t c,uint32_t cc){
+  int j = strips.numPixels();  
+    for(int i = 0; i < 4; i ++){
+      strips.setPixelColor(i,c);
+      strips.setPixelColor(j/2-(i+1),cc);
+      strips.setPixelColor(j/2+i,c);
+      strips.setPixelColor(j-(i+1),cc);
+      strips.show();
+      delay(wait);
+   } 
+}
+
+void doubleWipe(LPD8806 strips, uint32_t c, int cc, uint8_t wait){
+  int i;
+  int j = 15;
+  for (i=0; i < strips.numPixels(); i++) {
+    strips.setPixelColor(i, c);
+    strips.setPixelColor(j-i,cc);
+    strips.show();
+    delay(wait);
+  }
+}
+
+/*void scannerPAC(LPD8806 strips, int cycles,uint8_t wait) {
+  for(int cycled = 0; cycled < cycles; cycled++){
+  for(int x = 0; x < strips.numPixels();x++){
+    strips.setPixelColor(x,strips.Color(0,0,0));
+  }
+  strips.setPixelColor((strips.numPixels()/3)-1,strips.Color(127,127,127));
+  strips.setPixelColor((2*strips.numPixels()/3)-1,strips.Color(127,127,127));
+  strips.setPixelColor((strips.numPixels())-1,strips.Color(127,127,127));
+
+  int i, j, pos, dir;
+  pos = 0;
+  dir = 1;
+  for(i=0; i<((strips.numPixels()-1)*2 + 13); i++) {
+    if(dir > 0){
+      strips.setPixelColor(pos, strips.Color(127, 63, 0));
+      strips.setPixelColor(pos - 2, strips.Color(127, 0, 0));
+      strips.setPixelColor(pos - 3, strips.Color(127, 0, 127));
+      strips.setPixelColor(pos - 4, strips.Color(0, 127, 127));
+      strips.setPixelColor(pos - 5, strips.Color(127, 20, 0));
+      strips.setPixelColor(pos - 6, strips.Color(0,0,0));
+    }else{
+      strips.setPixelColor(pos - 6, strips.Color(127, 63, 0));
+      strips.setPixelColor(pos - 5, strips.Color(0,0,0));
+      strips.setPixelColor(pos - 4, strips.Color(127, 0, 0));
+      strips.setPixelColor(pos - 3, strips.Color(127, 0, 127));
+      strips.setPixelColor(pos - 2, strips.Color(0, 127, 127));
+      strips.setPixelColor(pos - 1, strips.Color(127, 20, 0));
+      strips.setPixelColor(pos, strips.Color(0,0,0));
+    }
+
+    strips.show();
+    delay(wait);
+    for(j=-2; j<= 2; j++) 
+      strips.setPixelColor(pos+j, strips.Color(0,0,0));
+    pos += dir;
+    if(pos < 0) {
+      pos = 1;
+      dir = -dir;
+    } 
+    else if(pos >= strips.numPixels()+6) {
+      pos = strips.numPixels()+6;
+      dir = -dir;
+    }
+  }
+  }
+}*/
+void binaryCount(long cycles, uint8_t wait, uint32_t c){
+  int bit1=0,bit2=0,bit3=0,bit4=0,bit5=0,bit6=0,bit7=0,bit8=0,bit9=0,bit10=0,bit11=0,bit12=0,bit13=0,bit14=0,bit15=0,bit16=0;
+  for( int i = 0; i< cycles ; i++){    
+    if(i%2 == 0){
+      strips.setPixelColor(0,c);
+      if(bit1 == 0) {
+        bit1=1;
+      } else {
+        bit1=0;
+      }
+    }else{
+      strips.setPixelColor(0,strips.Color(0,0,0));
+    }
+    if((i+bit1)%2 == 0){
+      strips.setPixelColor(1,c);
+    }else{
+      strips.setPixelColor(1,strips.Color(0,0,0));
+    }
+    if((i+1)%4 == 0){
+      if(bit3 == 1){
+        bit3 = 0;
+        strips.setPixelColor(2,strips.Color(0,0,0));
+      }else{
+        strips.setPixelColor(2,c);
+        bit3 = 1;        
+      } 
+    }
+    if((i+1)%8 == 0){
+      if(bit4 == 1){
+        strips.setPixelColor(3,strips.Color(0,0,0));
+        bit4 = 0;
+      }else{
+        strips.setPixelColor(3,c);
+        bit4 = 1;
+      }
+    }    
+    if((i+1)%16 == 0){
+      if(bit5 == 1){
+        strips.setPixelColor(4,strips.Color(0,0,0));
+        bit5 = 0;
+      }else{
+        strips.setPixelColor(4,c);
+        bit5 = 1;
+      }
+    }
+    if((i+1)%32 == 0){
+      if(bit6 == 1){
+        strips.setPixelColor(5,strips.Color(0,0,0));
+        bit6 = 0;
+      }else{
+        strips.setPixelColor(5,c);
+        bit6 = 1;
+      }
+    }
+    if((i+1)%64 == 0){
+      if(bit7 == 1){
+        strips.setPixelColor(6,strips.Color(0,0,0));
+        bit7 = 0;
+      }else{
+        strips.setPixelColor(6,c);
+        bit7 = 1;
+      }
+    }
+    if((i+1)%128 == 0){
+      if(bit8 == 1){
+        strips.setPixelColor(7,strips.Color(0,0,0));
+        bit8 = 0;
+      }else{
+        strips.setPixelColor(7,c);
+        bit8 = 1;
+      }
+    }
+    if((i+1)%256 == 0){
+      if(bit9 == 1){
+        strips.setPixelColor(8,strips.Color(0,0,0));
+        bit9 = 0;
+      }else{
+        strips.setPixelColor(8,c);
+        bit9 = 1;
+      }
+    }
+    if((i+1)%512 == 0){
+      if(bit10 == 1){
+        strips.setPixelColor(9,strips.Color(0,0,0));
+        bit10 = 0;
+      }else{
+        strips.setPixelColor(9,c);
+        bit10 = 1;
+      }
+    }
+    if((i+1)%1024 == 0){
+      if(bit11 == 1){
+        strips.setPixelColor(10,strips.Color(0,0,0));
+        bit11 = 0;
+      }else{
+        strips.setPixelColor(10,c);
+        bit11 = 1;
+      }
+    }
+    if((i+1)%2048 == 0){
+      if(bit12 == 1){
+        strips.setPixelColor(11,strips.Color(0,0,0));
+        bit12 = 0;
+      }else{
+        strips.setPixelColor(11,c);
+        bit12 = 1;
+      }
+    }
+    if((i+1)%4096 == 0){
+      if(bit13 == 1){
+        strips.setPixelColor(12,strips.Color(0,0,0));
+        bit13 = 0;
+      }else{
+        strips.setPixelColor(12,c);
+        bit13 = 1;
+      }
+    }
+    if((i+1)%8192 == 0){
+      if(bit14 == 1){
+        strips.setPixelColor(13,strips.Color(0,0,0));
+        bit14 = 0;
+      }else{
+        strips.setPixelColor(13,c);
+        bit14 = 1;
+      }
+    }
+    if((i+1)%16384 == 0){
+      if(bit15 == 1){
+        strips.setPixelColor(14,strips.Color(0,0,0));
+        bit15 = 0;
+      }else{
+        strips.setPixelColor(14,c);
+        bit15 = 1;
+      }
+    }
+    if((i+1)%32768 == 0){
+      if(bit16 == 1){
+        strips.setPixelColor(15,strips.Color(0,0,0));
+        bit16 = 0;
+      }else{
+        strips.setPixelColor(15,c);
+        bit16 = 1;
+      }
+    }
+    strips.show();
+    delay(wait);
+  }
+}
+
+
+/* Helper functions */
+
+//Input a value 0 to 384 to get a color value.
+//The colours are a transition r - g -b - back to r
+
+uint32_t Wheel(uint16_t WheelPos)
+{
+  byte r, g, b;
+  switch(WheelPos / 128)
+  {
+  case 0:
+    r = 127 - WheelPos % 128;   //Red down
+    g = WheelPos % 128;      // Green up
+    b = 0;                  //blue off
+    break; 
+  case 1:
+    g = 127 - WheelPos % 128;  //green down
+    b = WheelPos % 128;      //blue up
+    r = 0;                  //red off
+    break; 
+  case 2:
+    b = 127 - WheelPos % 128;  //blue down 
+    r = WheelPos % 128;      //red up
+    g = 0;                  //green off
+    break; 
+  }
+  return(strips.Color(r,g,b));
+}
+
+
+
+
+
+
